5a7b108ce53a190e90ff0d0f1fb569ddd2302017
Darian Lagman <darjaylag@gmail.com>
2026-05-03 22:59:26 -0500

    REFACTORED: enabled static export for Cloudflare Pages deployment

--- next.config.mjs
+++ next.config.mjs
@@ -1,10 +1,12 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  output: "export",
   reactStrictMode: true,
   experimental: {
     typedRoutes: false,
   },
   images: {
+    unoptimized: true,
     formats: ["image/avif", "image/webp"],
   },
 };
